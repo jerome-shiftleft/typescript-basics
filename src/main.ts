0bf95d0a5286861a3278dd844c9e1752e875c3e0
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-23 01:51:06 +0800

    progress

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 console.log('main.ts loaded!');
 
-function add(n1: number, n2: number) {
+function add(n1: any, n2: any) {
   console.log(`typeof number1: ${typeof n1}`);
   console.log(`typeof number2: ${typeof n2}`);
 
   if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    throw new Error('Input must be a number!');    
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
   }
   
   return n1 + n2;
@@ -14,8 +15,6 @@ function add(n1: number, n2: number) {
 let number1 = '5';
 let number2 = 2.8;
 
-number1 = parseFloat(number1);
-number2 = parseFloat(number2);
 
 const result = add(number1, number2);
 console.log(`number1 + number2 = ${result}`);
