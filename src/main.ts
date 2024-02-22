3d1b320623dbacf791347c06f256c6b8f2dcf07d
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-23 01:56:26 +0800

    progress

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 console.log('main.ts loaded!');
 
-function add(n1: any, n2: any) {
+function add(n1: number, n2: number) {
   console.log(`typeof number1: ${typeof n1}`);
   console.log(`typeof number2: ${typeof n2}`);
 
   if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    n1 = parseFloat(n1);
-    n2 = parseFloat(n2);
+    throw new Error('Incorrect input!');
   }
   
   return n1 + n2;
 }
 
-let number1 = '5';
+let number1 = 5;
 let number2 = 2.8;
 
 
