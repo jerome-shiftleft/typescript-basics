5ac1115f957f5b48623ad061b0791981e9abe0e1
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-22 02:12:48 +0800

    Fix formatting and add error handling in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-console.log('main.ts loaded!');
+console.log("main.ts loaded!");
 
 function add(n1: number, n2: number): number {
   console.log(`typeof number1: ${typeof n1}`);
   console.log(`typeof number2: ${typeof n2}`);
 
-  if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    throw new Error('Incorrect input!');
+  if (typeof n1 !== "number" || typeof n2 !== "number") {
+    throw new Error("Incorrect input!");
   }
-  
+
   return n1 + n2;
 }
 
 let number1 = 5;
 let number2 = 2.8;
-
 
 const result = add(number1, number2);
 console.log(`${number1} + ${number2} = ${result}`);
