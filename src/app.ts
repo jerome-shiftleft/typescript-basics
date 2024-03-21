f138e0c5e8a9b7728bc07fa559308553d941c5d5
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-22 03:05:27 +0800

    Add lecture files and import arrayTypes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,3 @@
-console.log("app.js loaded");
-
-type Person = {
-  name: string;
-  age: number;
-};
-
-const person: Person = {
-  name: "Jerome",
-  age: 36,
-};
-
-// const person: { name: string; age: number } = {
-//   name: "Jerome",
-//   age: 36,
-// };
-
-console.log(person.name);
+//import './lectures/basics';
+//import './lectures/objectTypes';
+import './lectures/arrayTypes'
---
+++ src/lectures/objectTypes.ts
@@ -0,0 +1,18 @@
+console.log("app.js loaded");
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+const person: Person = {
+  name: "Jerome",
+  age: 36,
+};
+
+// const person: { name: string; age: number } = {
+//   name: "Jerome",
+//   age: 36,
+// };
+
+console.log(person.name);
