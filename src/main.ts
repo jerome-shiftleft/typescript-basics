e885706f99eed5cb321b15622a01bb7435faa951
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-22 02:27:37 +0800

    Add phrase parameter to add function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 console.log("main.ts loaded!");
 
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   console.log(`typeof number1: ${typeof n1}`);
   console.log(`typeof number2: ${typeof n2}`);
 
@@ -9,7 +9,7 @@ function add(n1: number, n2: number, showResult: boolean) {
   }
 
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(`${phrase}${n1 + n2}`);
   } else {
     return n1 + n2;
   }
@@ -18,5 +18,6 @@ function add(n1: number, n2: number, showResult: boolean) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = "Result is: ";
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
