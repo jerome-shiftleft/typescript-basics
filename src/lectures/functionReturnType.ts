09b3fe2b771b064556533388bac4dfa08ef50407
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-22 21:03:11 +0800

    Add functionReturnType implementation

--- src/lectures/functionReturnType.ts
+++ src/lectures/functionReturnType.ts
@@ -1,3 +1,15 @@
 export default function functionReturnType() {
-  
-}
+
+  function printResult(result: number): void {
+    console.log(`result: ${result}`);
+  }
+
+  function add(n1: number, n2: number): number {
+    printResult(n1 + n2);
+    return n1 + n2;
+  }
+
+  add(1, 2.4);
+  //console.log(`result: ${result}`);
+
+} // end of export default function functionReturnType()
